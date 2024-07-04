d46d0720f31eb0e82242c29c1d9da0b7946e2353
G384462023 <238446@st.takushoku-u.ac.jp>
2024-07-04 16:05:49 +0900

    数当てゲームのリンクを設置

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -8,12 +8,14 @@ let result;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
 
+let b = document.querySelector('button#print');
+b.addEventListener('click', hantei); 
+let b1 = querySelector('input[name="yoso"]')
+let yoso = Number(b1.value);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
@@ -50,8 +52,8 @@ function hantei() {
     //4回目の追加分
     let kai = document.querySelector('span#kaisu'); 
     kai.textContent = kaisu; 
-    let kot = document.querySelector('span#answer'); 
-    kot.textContent = kotae;
+    let yos = document.querySelector('span#answer'); 
+    yos.textContent = yoso;
     let res = document.querySelector('p#result'); 
     res.textContent = result;
     //終わり
