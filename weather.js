c6f5e2a644f25a10dd9da9e23990592c9b0a4453
G384462023 <238446@st.takushoku-u.ac.jp>
2024-07-18 16:05:15 +0900

    数当ての位置変更

--- weather.js
+++ weather.js
@@ -46,7 +46,41 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
+console.log(data.coord);
+let ns = data.weather;
+for(let n of ns){
+  console.log(n);
+}
+console.log(data.base);
+console.log(data.main);
+console.log(data.visibility);
+console.log(data.wind);
+console.log(data.clouds);
+console.log(data.dt);
+console.log(data.sys);
+console.log(data.timezone);
+console.log(data.id);
+console.log(data.name);
+console.log(data.cod); 
+//print(data);
 
+//function print(data){
+//3回の課題内容
+//}
+//4回目の課題
 
 //5回目のプロジェクト課題
-
+let div = document.querySelector('div#result'); 
+let li = document.createElement('li'); 
+li.textContent = '緯度: '+(data.coord.lon); 
+div.insertAdjacentElement('beforeend',li); 
+let li1 = document.createElement('li'); 
+li1.textContent = '経度: '+(data.coord.lat); 
+div.insertAdjacentElement('beforeend',li1);
+ns=data.weather;
+let li2 = document.createElement('li'); 
+li2.textContent = '天気: '+(ns.description); 
+div.insertAdjacentElement('beforeend',li2); 
+let li3 = document.createElement('li'); 
+li3.textContent = '最低気温: '+(data.main.temp_min); 
+div.insertAdjacentElement('beforeend',li3); 
